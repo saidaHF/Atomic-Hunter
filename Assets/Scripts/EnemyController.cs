b0765b29bc8e9e7b6b28a1dec189fc30667e4fd7
Saida <saida.hf@hotmail.com>
2021-04-11 02:49:04 +0200

    Follow and Patrol enemies

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -11,17 +11,16 @@ public class EnemyController : MonoBehaviour {
   Transform playerPosition;
     // Start is called before the first frame update
     void Start() {
-        playerPosition = GameObject.Find("Lire").transform;
-        points = GameObject.FindGameObjectsWithTag("Point");
+        playerPosition = GameObject.Find("Lire").transform; 
         random = Random.Range(0, points.Length);
     }
     // Update is called once per frame
     void Update() {
       // Follow character
       if (follow) {
-          //transform.position = Vector2.MoveTowards(transform.position, EnemiePosition, speed * Time.deltaTime);
           transform.position = Vector2.MoveTowards(transform.position, playerPosition.position, speed * 1.5f * Time.deltaTime);
-      } else { // Patrol enemies 
+      } else { 
+        // Patrol enemies 
         transform.position = Vector2.MoveTowards(transform.position, points[random].transform.position, speed * Time.deltaTime);
         time += Time.deltaTime;
         if (time >= 2) {
@@ -30,7 +29,6 @@ public class EnemyController : MonoBehaviour {
         }
       }
     }
-
     private void OnTriggerEnter2D(Collider2D collision) {
       if (collision.gameObject.tag == "Player") {
           follow = true;
